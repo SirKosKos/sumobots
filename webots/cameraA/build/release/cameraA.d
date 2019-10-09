d4ea37f2def82f488870b8d89431297524656fd6
Kostas <44584727+SirKosKos@users.noreply.github.com>
2019-10-10 02:59:00 +0300

    Delete cameraA.d

--- webots/cameraA/build/release/cameraA.d
+++
@@ -1,8 +0,0 @@
-build/release/cameraA.o: cameraA.c \
- /usr/local/webots/include/controller/c/webots/camera.h \
- /usr/local/webots/include/controller/c/webots/camera_recognition_object.h \
- /usr/local/webots/include/controller/c/webots/types.h \
- /usr/local/webots/include/controller/c/webots/motor.h \
- /usr/local/webots/include/controller/c/webots/robot.h \
- /usr/local/webots/include/controller/c/webots/nodes.h \
- /usr/local/webots/include/controller/c/webots/utils/system.h
